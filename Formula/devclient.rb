fde78553f9180a1c18b8bd8133195eba10ecb99b
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2019-12-03 15:23:55 +0100

    Add version for head param, fix dependency params

--- Formula/devclient.rb
+++ Formula/devclient.rb
@@ -1,17 +1,17 @@
 class Devclient < Formula
 	desc "Conclusive Development Cable Client"
 	homepage "https://github.com/conclusiveeng/devclient"
-	head "https://github.com/conclusiveeng/devclient.git"
+	head "https://github.com/conclusiveeng/devclient.git", tag => "0.1.2"
 
-	depends_on "cmake" => :build
-	depends_on "libtool" => :build
-	depends_on "automake" => :build
-	depends_on "libusb" => :build
-	depends_on "libusb-compat" => :build
-	depends_on "hidapi" => :build
-	depends_on "libftdi" => :build
-	depends_on "gtkmm3" => :build
-	depends_on "telnet" => :build
+	depends_on "cmake"
+	depends_on "libtool"
+	depends_on "automake"
+	depends_on "libusb"
+	depends_on "libusb-compat"
+	depends_on "hidapi"
+	depends_on "libftdi"
+	depends_on "gtkmm3"
+	depends_on "telnet"
 
 	def install
 		system "mkdir -pv build && cd build && cmake .. && make"
